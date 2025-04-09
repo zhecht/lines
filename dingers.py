5935c5f9b20f730ebb2e63703b7016f1f1b6c33c
Zack Hecht <zhecht7@gmail.com>
2025-04-09 13:39:24 -0400

    test

--- dingers.py
+++ dingers.py
@@ -276,7 +276,7 @@ def writeCirca(date):
 			x = box.crop((w-110,40,w-60,h))
 
 			player_img = box.crop((0,0,w,40)) # l,t,r,b
-			player = pytesseract.image_to_string(x).split("\n")[0]
+			player = pytesseract.image_to_string(player_img).split("\n")[0]
 			line = str(float(pytesseract.image_to_string(x).split("\n")[0][0]) + 0.5)
 			print(player, line)
 
