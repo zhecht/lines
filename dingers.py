2a8b993e84038564370a3c59366a6e1c25eb7e12
Zack Hecht <zhecht7@gmail.com>
2025-04-09 13:49:56 -0400

    test

--- dingers.py
+++ dingers.py
@@ -298,7 +298,7 @@ def writeCirca(date):
 				player = parsePlayer(player.lower().split(" (")[0])
 
 				data[game]["k"][player][line] = f"{o}/{u}"
-				boxT += h
+				boxT += h+5
 
 	with open("static/mlb/circa-props.json", "w") as fh:
 		json.dump(data, fh, indent=4)
