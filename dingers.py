c6479fe053144df5b18838cd5db031c385dfea59
Zack Hecht <zhecht7@gmail.com>
2025-04-14 16:36:43 -0400

    test

--- dingers.py
+++ dingers.py
@@ -349,7 +349,7 @@ def writeCirca(date):
 					team = convertMGMTeam(team[0])
 					game = teamGame.get(team, "")
 
-					i = box.crop((70,30,207,h))
+					i = box.crop((67,30,207,h))
 					i.save(f"out-{row}.png", "PNG")
 					line = pytesseract.image_to_string(i).split("\n")
 					line = [x for x in line if x.replace("\x0c", "")]
