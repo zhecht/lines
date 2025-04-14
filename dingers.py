daeb5de070c4061746240bcedccd3a344ff445f5
Zack Hecht <zhecht7@gmail.com>
2025-04-14 16:32:42 -0400

    test

--- dingers.py
+++ dingers.py
@@ -352,6 +352,7 @@ def writeCirca(date):
 					i = box.crop((70,30,207,h))
 					i.save(f"out-{row}.png", "PNG")
 					line = pytesseract.image_to_string(i).split("\n")
+					print(line)
 					line = [x for x in line if x.replace("\x0c", "")]
 
 					if not line:
