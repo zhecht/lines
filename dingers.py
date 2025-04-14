204e944870aa8296bd1bc4444008928f421c577a
Zack Hecht <zhecht7@gmail.com>
2025-04-14 15:56:12 -0400

    test

--- dingers.py
+++ dingers.py
@@ -337,7 +337,7 @@ def writeCirca(date):
 
 					i = box.crop((0,0,w,25))
 					team = pytesseract.image_to_string(i).split("\n")
-					team = convertMLBTeam(team[0])
+					team = convertMGMTeam(team[0])
 					print(team)
 					#box.save(f"out-0.png", "PNG")
 
