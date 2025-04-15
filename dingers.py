3cea9034a20101d919df20da2c756ad860f758e8
Zack Hecht <zhecht7@gmail.com>
2025-04-15 12:20:03 -0400

    test

--- dingers.py
+++ dingers.py
@@ -177,6 +177,7 @@ def writeCircaMain(date):
 			total_img = img.crop((820,top+add+5,970,top+97+add-5))
 			total_img.save(f"out-total-{i}.png", "PNG")
 			total_text = pytesseract.image_to_string(total_img).split("\n")
+			print(total_text)
 			add += 97
 			if not total_text[0] or not total_text[1]:
 				totals.extend([None, None])
