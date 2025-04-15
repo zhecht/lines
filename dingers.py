c72597ff4b6b87f96a477fe060ce41379ec566f6
Zack Hecht <zhecht7@gmail.com>
2025-04-15 13:10:40 -0400

    test

--- dingers.py
+++ dingers.py
@@ -313,6 +313,8 @@ def writeCirca(date):
 			o = re.search(r"\d{3,4}", under)
 			if not o:
 				continue
+			elif len(o) == 4 and "-" not in o:
+				o = "-"+o
 			unders.append(under)
 		
 		for p,o,u in zip(players, overs, unders):
