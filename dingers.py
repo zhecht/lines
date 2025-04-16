4aeed25d86b3bbc0cd69b346dedb710c65a2a536
Zack Hecht <zhecht7@gmail.com>
2025-04-16 02:59:30 -0400

    test

--- dingers.py
+++ dingers.py
@@ -844,7 +844,8 @@ def writeHistory():
 	bookData = nested_dict()
 	for date, games in oddsHist.items():
 		for game in games:
-			print(game)
+			if not game:
+				continue
 			a,h = map(str, game.split(" @ "))
 			for player, books in oddsHist[date][game].items():
 				team = ""
