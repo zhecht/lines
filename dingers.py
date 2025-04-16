bda07fada8a573671b0dd361699ad60410e14e7c
Zack Hecht <zhecht7@gmail.com>
2025-04-16 03:45:37 -0400

    test

--- dingers.py
+++ dingers.py
@@ -117,7 +117,9 @@ def writeCircaHistory(date):
 		a,h = map(str, game.split(" @ "))
 		teamGame[a] = game
 		teamGame[h] = game
-		
+
+	print(teamGame)
+
 	dt = datetime.strftime(datetime.strptime(date, "%Y-%m-%d"), "%Y-%-m-%-d")
 	file = f"/mnt/c/Users/zhech/Downloads/MLB Props - {dt}.pdf"
 	if not os.path.exists("/mnt/c/Users"):
