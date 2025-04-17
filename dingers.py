ab36e0604665a81f166648baaf3d3d0a19cdd9bc
Zack Hecht <zhecht7@gmail.com>
2025-04-17 10:51:30 -0400

    test

--- dingers.py
+++ dingers.py
@@ -190,7 +190,7 @@ def writeCircaMain(date):
 		page.save("out.png", "PNG")
 		img = Image.open("out.png")
 		bottom, top = 1930, 480
-		if pageIdx == 0:
+		if pageIdx:
 			bottom, top = 1550, 500
 		#w,h = img.size
 		# l,t,r,b
