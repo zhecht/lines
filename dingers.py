66458d3500589b6a8bdf2fd4c66e4487a7042bb8
Zack Hecht <zhecht7@gmail.com>
2025-04-22 01:52:58 -0400

    test

--- dingers.py
+++ dingers.py
@@ -1144,6 +1144,10 @@ def analyzeHistory():
 			data[player][f"{book}_avg"] = averageOdds(odds)
 			data[player][f"{book}_median"] = sorted(odds)[len(odds) // 2]
 
+			if player == "oneil cruz":
+				print(odds)
+				print(averageOdds(odds))
+
 			for k in ["avg_vig", "avg", "median"]:
 				debug[player][f"{book}_{k}"] = data[player][f"{book}_{k}"]
 
