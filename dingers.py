9b8a45cee6490334125977a14ce483f63a2c51db
Zack Hecht <zhecht7@gmail.com>
2025-05-05 17:50:07 -0400

    test

--- dingers.py
+++ dingers.py
@@ -248,7 +248,7 @@ def writeCircaMain(date):
 					game = f"{convertMGMTeam(text[i])} @ {convertMGMTeam(text[i+1])}"
 				except:
 					break
-				data[game]["rfi"] = f"{rfi_text[i]}/{rfi_text[i+1]}".replace("=", "-")
+				data[date][game]["rfi"] = f"{rfi_text[i]}/{rfi_text[i+1]}".replace("=", "-")
 			continue
 
 		playersImg = img.crop((300,top,525,bottom))
