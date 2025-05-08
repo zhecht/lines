b935bbcb27826864c0e108402d5e3150a415edbf
Zack Hecht <zhecht7@gmail.com>
2025-05-08 11:57:16 -0400

    test

--- dingers.py
+++ dingers.py
@@ -396,7 +396,7 @@ def writeCirca(date):
 	for pageIdx, page in enumerate(pages):
 		page.save(f"out-{pageIdx}.png", "PNG")
 		img = Image.open(f"out-{pageIdx}.png")
-		bottom, top = 2066, 400
+		bottom, top = 2370, 400
 		left, right = 105, 415
 
 		if pageIdx == 1:
